35c8e5134608097c85bfa7f29d5b3c97c9e5573a
Jean Pierre <jp.saldana@outlook.es>
2022-10-01 11:25:00 -0500

    Reservation: Routes resource, model New y Create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,20 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to reservation_path(@reservation)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date_start, :date_finish,:status, :start_time, :finish_time, :place_id, :user_id)
+  end
 end
