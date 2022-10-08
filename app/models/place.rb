52a8f2f559c7b0c5b3e2c4c93a28304d89c55ced
Eryck Milano <eryckmilano@gmail.com>
2022-10-08 15:53:13 -0300

    cambios menores

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :reservations
-  validates :city, :area, :price, :address, presence: true
+  validates :city, :area, :price, :description, :address, presence: true
   has_many :reviews, dependent: :destroy
   has_many_attached :photos
 end
